bec7225c6dd2bb349e0a9f51deb00fb2b4c0ae95
Wallys Ferreira <wallysfsilva@gmail.com>
2025-08-18 19:59:15 +0000

    use snake_case convention for variable names

--- src/config_parser.c
+++ src/config_parser.c
@@ -3,27 +3,27 @@
 #include "types.h"
 #include "config_parser.h"
 
-ParseResultCode error(const char *error_message, ParseResultCode errorCode) {
+ParseResultCode error(const char *error_message, ParseResultCode error_code) {
     fprintf(stderr, "%s\n", error_message);
-    return errorCode;
+    return error_code;
 }
 
-ParseResultCode parse_process_id(cJSON *processItemJson, Config *config) {
-    cJSON *processIdJson = cJSON_GetObjectItem(processItemJson, "id");
-    if (cJSON_IsNumber(processIdJson)) {
-        printf("Process id = %d\n", processIdJson->valueint);
-        config->id = processIdJson->valueint;
+ParseResultCode parse_process_id(cJSON *process_item_json, Config *config) {
+    cJSON *process_id_json = cJSON_GetObjectItem(process_item_json, "id");
+    if (cJSON_IsNumber(process_id_json)) {
+        printf("Process id = %d\n", process_id_json->valueint);
+        config->id = process_id_json->valueint;
         return PARSE_OK;
     } else {
         return error("Expected \"id\" field to be a number", PARSE_ID_NOT_NUMBER_ERROR);
     }
 }
 
-ParseResultCode parse_process_arrival(cJSON *processItemJson, Config *config) {
-    cJSON *arrivalJson = cJSON_GetObjectItem(processItemJson, "arrival");
-    if (cJSON_IsNumber(arrivalJson)) {
-        printf("Process arrival = %d\n", arrivalJson->valueint);
-        config->arrival = arrivalJson->valueint;
+ParseResultCode parse_process_arrival(cJSON *process_item_json, Config *config) {
+    cJSON *arrival_json = cJSON_GetObjectItem(process_item_json, "arrival");
+    if (cJSON_IsNumber(arrival_json)) {
+        printf("Process arrival = %d\n", arrival_json->valueint);
+        config->arrival = arrival_json->valueint;
         return PARSE_OK;
     } else {
         return error("Expected \"arrival\" field to be a number", PARSE_ERROR);
@@ -42,20 +42,20 @@ ParseResultCode parse_process_item(cJSON *processItemJson, Config *config) {
     }
 }
 
-ParseResultCode parse_processes_array(cJSON *jsonRoot, Config *config) {
-    cJSON *processesJson = cJSON_GetObjectItem(jsonRoot, "processes");
-    if (processesJson == NULL) {
+ParseResultCode parse_processes_array(cJSON *json_root, Config *config) {
+    cJSON *processes_json = cJSON_GetObjectItem(json_root, "processes");
+    if (processes_json == NULL) {
         return error("\"processes\" field should exist", PARSE_NO_PROCESSES_FIELD_ERROR);
     }
 
-    if (cJSON_IsArray(processesJson)) {
-        if (cJSON_GetArraySize(processesJson) == 0) {
+    if (cJSON_IsArray(processes_json)) {
+        if (cJSON_GetArraySize(processes_json) == 0) {
             return error("\"processes\" should not be empty", PARSE_EMPTY_PROCESSES_ERROR);
         }
 
-        for (int i = 0; i < cJSON_GetArraySize(processesJson); i++) {
-            cJSON *processItemJson = cJSON_GetArrayItem(processesJson, i);
-            return parse_process_item(processItemJson, config);
+        for (int i = 0; i < cJSON_GetArraySize(processes_json); i++) {
+            cJSON *process_item_json = cJSON_GetArrayItem(processes_json, i);
+            return parse_process_item(process_item_json, config);
         }
     } else {
         return error("Expected \"processes\" field to be an array", PARSE_PROCESSES_NOT_ARRAY_ERROR);
@@ -63,13 +63,13 @@ ParseResultCode parse_processes_array(cJSON *jsonRoot, Config *config) {
 }
 
 ParseResultCode parse(const char* data, Config *config) {
-    cJSON *jsonRoot = cJSON_Parse(data);
-    if (jsonRoot == NULL) {
+    cJSON *json_root = cJSON_Parse(data);
+    if (json_root == NULL) {
         return error("Something went wrong when parsing json", PARSE_INVALID_JSON_ERROR);
     }
 
-    ParseResultCode result = parse_processes_array(jsonRoot, config);
+    ParseResultCode result = parse_processes_array(json_root, config);
 
-    cJSON_Delete(jsonRoot);
+    cJSON_Delete(json_root);
     return result;
 }
